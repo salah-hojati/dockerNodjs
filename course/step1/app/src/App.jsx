82527480a94445d7fc01e7527095ba86a8976279
salah <salahedin1362@gmail.com>
2025-07-24 10:30:07 +0330

    external style sheet

--- course/step1/app/src/App.jsx
+++ course/step1/app/src/App.jsx
@@ -1,7 +1,17 @@
+// HOW TO STYLE REACT COMPONENTS WHIT CSS
+// ----------------
+// (not including external frameworks or preprocessor)
+
+// 1. EXTERNAL
+// 2. MODULES
+// 3.INLINE
+import Button from "./Button.jsx";
 
 function App() {
     return (
-        <></>
+        <>
+        <Button/>
+        </>
     );
 
 }
