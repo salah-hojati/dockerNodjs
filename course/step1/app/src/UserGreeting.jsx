e3dcca97603e06d7c5ebd70369031dbea8034e71
salah <salahedin1362@gmail.com>
2025-07-25 11:24:37 +0330

    props 4.3

--- course/step1/app/src/UserGreeting.jsx
+++ course/step1/app/src/UserGreeting.jsx
@@ -1,19 +1,15 @@
 function UserGreeting(props) {
-    /*    if (props.isLoggedIn) {
-            return (<h2>Welcome {props.username} </h2>);
-        } else {
-            return (<h2>Please log in to continue</h2>)
-        }*/
+    const welcomeMessage = <h2 className="welcome-message">
+        Welcome {props.username}
+    </h2>
 
-    /*    if (props.isLoggedIn) {
-            return (<h2>Welcome {props.username} </h2>);
-        }
-            return (<h2>Please log in to continue</h2>)*/
+    const loginPrompt = <h2 className="login-prompt">
+        Please log in to continue
+    </h2>
 
 
-    return (props.isLoggedIn ?
-        <h2 className="welcome-message">Welcome {props.username} </h2> :
-        <h2 className="login-prompt">Please log in to continue</h2>)
+    return (props.isLoggedIn ? welcomeMessage :loginPrompt
+    )
 }
 
 export default UserGreeting
