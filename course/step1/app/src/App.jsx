dd3fc3613b5dc0ee58fdcc28d488b7cedb11a608
salah <salahedin1362@gmail.com>
2025-07-25 10:07:10 +0330

    props

--- course/step1/app/src/App.jsx
+++ course/step1/app/src/App.jsx
@@ -1,7 +1,14 @@
-
+//props = read-only properties that over shared components.
+//        A parent component can send date to a child component.
+//        <Component key=value>
+import Student from "./Student.jsx";
 function App() {
     return (
-        <></>
+        <>
+        <Student name="Spongebob" age={32} isStudent={true} />
+        <Student name="Patrick" age={42} isStudent={false} />
+        <Student name="squidward" age={50} isStudent={false} />
+        </>
     );
 
 }
---
+++ course/step1/app/src/Student.jsx
@@ -0,0 +1,20 @@
+// propTypes = a mechanism that ensure that the passed value
+//               is of the correct datatype.
+//     age: PropTypes.number
+
+
+
+
+
+
+function Student(props) {
+    return (
+        <div className="student">
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age} </p>
+            <p>Student: {props.isStudent ?"Yes":"No"} </p>
+        </div>
+    );
+}
+
+export default Student
